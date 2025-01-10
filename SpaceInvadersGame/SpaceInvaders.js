5f61190274dede1127342bb1885068771a6c00a0
Fmcivor <mcivorfintan@gmail.com>
2025-01-10 16:31:03 +0000

    Adjustments of header for iphone

---
+++ SpaceInvadersGame/SpaceInvaders.js
@@ -0,0 +1,8 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementsByClassName('GameCanvas')[0];
+
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = '#f50000';
+
+ctx.fillRect(0,100,100,100);

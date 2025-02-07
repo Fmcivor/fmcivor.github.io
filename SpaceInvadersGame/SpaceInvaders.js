b6fa6dfbbf8bfc98eedd87a38200c8da1f8c2cb9
Fmcivor <mcivorfintan@gmail.com>
2025-02-07 14:08:06 +0000

    Project page scrollable sidebar remains fixed but main content scrolls

--- SpaceInvadersGame/SpaceInvaders.js
+++
@@ -1,8 +0,0 @@
-/** @type {HTMLCanvasElement} */
-const canvas = document.getElementsByClassName('GameCanvas')[0];
-
-const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = '#f50000';
-
-ctx.fillRect(0,100,100,100);
